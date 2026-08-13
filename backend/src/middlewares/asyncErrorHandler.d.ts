12587b9c799dfd6f1f9c53531417d631a946ddd3
Niraj3004 <nirajkushwaha84@gmail.com>
2026-08-13 18:30:51 +0545

    chore: remove unwanted compiled files from src directory

--- backend/src/middlewares/asyncErrorHandler.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const asyncErrorHandler: (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) => (req: Request, res: Response, next: NextFunction) => void;
-//# sourceMappingURL=asyncErrorHandler.d.ts.map
--- backend/src/utils/response.d.ts
+++
@@ -1,3 +0,0 @@
-import { Response } from 'express';
-export declare const sendResponse: (res: Response, statusCode: number, success: boolean, message: string, data?: any) => Response<any, Record<string, any>>;
-//# sourceMappingURL=response.d.ts.map
